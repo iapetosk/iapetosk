3f4a5f4b22bd8fd5b8b66fe861bc6e8bb75f83b8
Sombian <motosolar@naver.com>
2020-09-14 15:29:26 +0900

    (build.js): fix typo

--- src/assets/loaders/jmana.ts
+++ src/assets/loaders/jmana.ts
@@ -17,8 +17,8 @@ class JMana {
 		return new Promise<Loaded>(async (resolve, rejects) => {
 			request.get(url).then((callback) => {
 				const list: string[][] = [
-					utility.parse(callback.content.encode, ".view > li > img:nth-child(1)", "data-src") as string[],
-					utility.parse(callback.content.encode, ".view > li > img:nth-child(1)", "src") as string[]
+					utility.parse(callback.content.encode, ".view > li > img:nth-child(1)", "src") as string[],
+					utility.parse(callback.content.encode, ".view > li > img:nth-child(1)", "data-src") as string[]
 				];
 				for (let x: number = 0; x < list.length; x++) {
 					for (let y: number = 0; y < list[x].length; y++) {
