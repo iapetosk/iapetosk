2006b0bbc1eb109acaf5002bf33c201e75188c7b
Sombian <motosolar@naver.com>
2020-09-19 12:38:31 +0900

    (iterable.tsx): featured buttons

--- src/renderer/components/titlebar.tsx
+++ src/renderer/components/titlebar.tsx
@@ -46,20 +46,20 @@ class TitleBar extends React.Component<TitleBarState, any> {
 						this.app.minimize();
 					}}
 					dangerouslySetInnerHTML={{ __html: require(`!html-loader!@/assets/icons/focus.svg`) }}>
-				</button >
+				</button>
 				<button id="restore" className="none-draggable"
 					onClick={() => {
 						this.setState({ ...this.state, restore: !this.state.restore });
 					}}
 					dangerouslySetInnerHTML={{ __html: require(`!html-loader!@/assets/icons/${this.state.restore ? `minimize` : `maximize`}.svg`) }}>
-				</button >
+				</button>
 				<button id="close" className="none-draggable"
 					onClick={() => {
 						this.app.close();
 					}}
 					dangerouslySetInnerHTML={{ __html: require(`!html-loader!@/assets/icons/close.svg`) }}>
-				</button >
-			</header >
+				</button>
+			</header>
 		);
 	}
 }
