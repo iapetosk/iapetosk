1923a7384ba8af64fbb056b79696734e2bdae7bd
Sombian <motosolar@naver.com>
2020-10-16 12:03:32 +0900

    (iterable.tsx): fix scroll height

--- src/renderer/components/treeview.tsx
+++ src/renderer/components/treeview.tsx
@@ -66,8 +66,8 @@ class TreeView extends React.Component<TreeViewState, any> {
 		path = path instanceof Array ? path[0] : path;
 		// for unknown reason, sometimes path is undefined
 		if (path) {
-			path = path.startsWith("//") ? path : [hostname, path].join("/");
-			path = path.startsWith("http") || path.startsWith("https") ? path : `https://${path}`;
+			path = new RegExp(/^\/\/?/).test(path) ? path : [hostname, path].join("/");
+			path = new RegExp(/^https?/).test(path) ? path : `https://${path}`;
 		}
 		return path;
 	}
