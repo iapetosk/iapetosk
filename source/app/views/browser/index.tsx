f6fd4f1de269a29d3befc2cd23d1f537d0e135c0
Sombian <motosolar@naver.com>
2020-12-12 23:40:57 +0900

    (browser.tsx): fix incorrect paging index

--- source/app/views/browser/index.tsx
+++ source/app/views/browser/index.tsx
@@ -11,7 +11,6 @@ import filter from "@/modules/hitomi/filter";
 import search from "@/modules/hitomi/search";
 import suggest from "@/modules/hitomi/suggest";
 import settings from "@/modules/configure";
-import listener from "@/modules/listener";
 import utility from "@/modules/utility";
 
 import { QueryProps } from "@/app/components/query";
@@ -26,7 +25,6 @@ export enum BrowserEvent {
 	SESSION = "browser.static.session",
 	BLOCKS = "browser.static.blocks"
 };
-
 export type BrowserProps = {
 	enable: boolean;
 };
@@ -47,80 +45,103 @@ class Browser extends React.Component<BrowserProps> {
 	constructor(props: BrowserProps) {
 		super(props);
 		this.props = props;
-		this.state = { query: { enable: true, options: { input: settings.query.input }, handler: { keydown: (value: string) => { listener.emit(BrowserEvent.QUERY, value); } } }, iterable: { blocks: [] }, paging: { enable: true, options: { size: 0, index: 0, metre: settings.paging.metre }, handler: { click: (value: number) => { listener.emit(BrowserEvent.PAGING, { ...this.state.paging, options: { ...this.state.paging.options, index: value } }); } } }, session: { history: [], version: 0 }, blocks: [[], 0] };
+		this.state = { query: { enable: true, options: { input: settings.query.input }, handler: { keydown: (value: string) => { this.mutate(BrowserEvent.QUERY, { ...this.state.query, options: { ...this.state.query.options, input: value } }); } } }, iterable: { blocks: [] }, paging: { enable: true, options: { size: 0, index: 0, metre: settings.paging.metre }, handler: { click: (value: number) => { this.mutate(BrowserEvent.PAGING, { ...this.state.paging, options: { ...this.state.paging.options, index: value } }); } } }, session: { history: [], version: 0 }, blocks: [[], 0] };
 
-		listener.on(BrowserEvent.SESSION, ($new: { history: [string, number][], version: number; }) => {
-			this.setState({ ...this.state, session: $new }, () => {
-				listener.emit(BrowserEvent.BLOCKS, [[], 0]);
-				search.get(filter.get($new.history[$new.version][0]), $new.history[$new.version][1], settings.hitomi.per_page).then(({ array, size, singular }) => {
-					const blocks = new Array<GalleryBlock>(Math.min(size - settings.hitomi.per_page * $new.history[$new.version][1], settings.hitomi.per_page));
-					for (let index = 0; index < blocks.length; index++) {
-						read.block(array[index + (singular ? 0 : $new.history[$new.version][1] * settings.hitomi.per_page)]).then((block) => {
-							blocks[index] = block;
-							if (Object.keys(blocks).length === blocks.length) {
-								listener.emit(BrowserEvent.BLOCKS, [blocks, size]);
-							}
-						});
+		window.addEventListener("keydown", (event) => {
+			if (this.props.enable && !document.querySelectorAll("input:focus").length) {
+				switch (event.key) {
+					case "ArrowLeft": {
+						this.mutate(BrowserEvent.PAGING, { ...this.state.paging, options: { ...this.state.paging.options, index: utility.clamp(this.state.paging.options.index - 1, 0, this.state.paging.options.size - 1) } });
+						break;
 					}
-				});
-			});
+					case "ArrowRight": {
+						this.mutate(BrowserEvent.PAGING, { ...this.state.paging, options: { ...this.state.paging.options, index: utility.clamp(this.state.paging.options.index + 1, 0, this.state.paging.options.size - 1) } });
+						break;
+					}
+				}
+			}
 		});
-		listener.on(BrowserEvent.BLOCKS, ($new: [GalleryBlock[], number]) => {
-			this.setState({
-				...this.state,
-				blocks: $new,
-				...($new[1] > 0 ? {
-					query: {
-						...this.state.query,
-						enable: true,
-					},
-					iterable: {
-						blocks: $new[0]
-					},
-					paging: {
-						...this.state.paging,
-						enable: true,
-						options: {
-							...this.state.paging.options,
-							size: ~~($new[1] / settings.hitomi.per_page)
+	}
+	public mutate(section: BrowserEvent, $new: any) {
+		switch (section) {
+			case BrowserEvent.SESSION: {
+				this.setState({ ...this.state, session: $new }, () => {
+					this.mutate(BrowserEvent.BLOCKS, [[], 0]);
+					search.get(filter.get($new.history[$new.version][0]), $new.history[$new.version][1], settings.hitomi.per_page).then(({ array, size, singular }) => {
+						const blocks = new Array<GalleryBlock>(Math.min(size - settings.hitomi.per_page * $new.history[$new.version][1], settings.hitomi.per_page));
+						for (let index = 0; index < blocks.length; index++) {
+							read.block(array[index + (singular ? 0 : $new.history[$new.version][1] * settings.hitomi.per_page)]).then((block) => {
+								blocks[index] = block;
+								if (Object.keys(blocks).length === blocks.length) {
+									this.mutate(BrowserEvent.BLOCKS, [blocks, size]);
+								}
+							});
 						}
-					}
-				} : {
+					});
+				});
+				break;
+			}
+			case BrowserEvent.BLOCKS: {
+				this.setState({
+					...this.state,
+					blocks: $new,
+					...($new[1] > 0 ? {
 						query: {
 							...this.state.query,
-							enable: false,
+							enable: true,
 						},
 						iterable: {
-							blocks: []
+							blocks: $new[0]
 						},
 						paging: {
 							...this.state.paging,
-							enable: false
+							enable: true,
+							options: {
+								...this.state.paging.options,
+								size: ~~($new[1] / settings.hitomi.per_page)
+							}
 						}
-					})
-			});
-		});
-		listener.on(BrowserEvent.QUERY, ($new: QueryProps) => {
-			this.setState({ ...this.state, query: $new }, () => {
-				listener.emit(BrowserEvent.SESSION, { history: [...this.state.session.history, [$new.options.input, 0]], version: this.state.session.version + 1 });;
-			});
-			suggest.up();
-			settings.query = { ...settings.query, input: $new.options.input }
-		});
-		listener.on(BrowserEvent.ITERABLE, ($new: IterableProps) => {
-			this.setState({ ...this.state, iterable: $new });
-		});
-		listener.on(BrowserEvent.PAGING, ($new: PagingProps) => {
-			this.setState({ ...this.state, paging: $new }, () => {
-				listener.emit(BrowserEvent.SESSION, { history: [...this.state.session.history, [settings.query.input, $new.options.index]], version: this.state.session.version + 1 });
-			});
-		});
-	}
-	public componentDidMount() {
-		listener.emit(BrowserEvent.SESSION, { history: [[settings.query.input, 0]], version: 0 });
+					} : {
+							query: {
+								...this.state.query,
+								enable: false,
+							},
+							iterable: {
+								blocks: []
+							},
+							paging: {
+								...this.state.paging,
+								enable: false
+							}
+						})
+				});
+				break;
+			}
+			case BrowserEvent.QUERY: {
+				this.setState({ ...this.state, query: $new }, () => {
+					this.mutate(BrowserEvent.PAGING, { ...this.state.paging, options: { ...this.state.paging.options, index: 0 } });
+				});
+				suggest.up();
+				settings.query = { ...settings.query, input: $new.options.input };
+				break;
+			}
+			case BrowserEvent.ITERABLE: {
+				this.setState({ ...this.state, iterable: $new });
+				break;
+			}
+			case BrowserEvent.PAGING: {
+				this.setState({ ...this.state, paging: $new }, () => {
+					this.mutate(BrowserEvent.SESSION, { history: [...this.state.session.history, [this.state.query.options.input, $new.options.index]], version: this.state.session.version + 1 });
+				});
+				break;
+			}
+		}
 	}
 	static getDerivedStateFromProps($new: BrowserProps, $old: BrowserProps) {
 		return $new;
+	}
+	public componentDidMount() {
+		this.mutate(BrowserEvent.SESSION, { history: [[settings.query.input, 0]], version: 0 });
 	}
 	public render() {
 		return (
